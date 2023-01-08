61cc7b6301fa6304face8944342259a3c15915b8
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-01-08 10:39:04 -0800

    Update LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp

--- Backtracking/LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Backtracking/LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,6 +1,5 @@
-Link to the problem: https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/
-
 /*
+    Link to the problem: https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/
     You are given an array of integers nums and an integer target.
     Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 10^9 + 7.
 */
