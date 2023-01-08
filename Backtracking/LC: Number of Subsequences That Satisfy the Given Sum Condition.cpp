fa20aae8c3f777ca0552e78bcf22b715c3dfa1e1
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-01-08 10:39:27 -0800

    Update LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp

--- Backtracking/LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Backtracking/LC: Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -4,8 +4,6 @@
     Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 10^9 + 7.
 */
 
-Solution:
-
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
