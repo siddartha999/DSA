ac7a9ce44053e1dd79dff0929d4f934ba240552b
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-02-06 04:50:22 -0800

    Create (4*): Path Sum III.cpp

---
+++ Trees/(4*): Path Sum III.cpp
@@ -0,0 +1,37 @@
+/**
+* Link: https://leetcode.com/problems/path-sum-iii/description/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        int count = 0;
+        unordered_map<int, int> cache;
+        cache[0] = 1;
+        helper(root, targetSum, 0, count, cache);
+        return count;
+    }
+
+    void helper(TreeNode* root, int target, long long pathSum, int& count, unordered_map<int, int>& cache) {
+        if(!root) return;
+        pathSum = (pathSum + root->val) % 100000009;
+        if(cache.find(pathSum - target) != cache.end()) count += cache[pathSum - target];
+        cache[pathSum]++;
+        helper(root->left, target, pathSum, count, cache);
+        helper(root->right, target, pathSum, count, cache);
+        cache[pathSum]--;
+    }
+};
+
+// TC: O(N), SC: O(N) post the cache space optimization!
