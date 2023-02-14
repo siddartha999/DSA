a914cbeb891bb5d4a3261bcd0e909b4d25b06ed5
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-02-14 04:41:55 -0800

    Update (4*): Two Sum IV: Input is a BST.cpp

--- Trees/(4*): Two Sum IV: Input is a BST.cpp
+++ Trees/(4*): Two Sum IV: Input is a BST.cpp
@@ -1,6 +1,8 @@
 /*
 * Problem: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
 * Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+
+*NOTE: Practice BST iterator problem first.
 */
 
 /**
