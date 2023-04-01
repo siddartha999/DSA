673437af1c943a1147b2a29feff044167ea8e9dc
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-04-01 16:37:19 -0700

    Update (4*): Delete Node in a BST.cpp

--- Trees/(4*): Delete Node in a BST.cpp
+++ Trees/(4*): Delete Node in a BST.cpp
@@ -2,21 +2,11 @@
 Link: https://leetcode.com/problems/delete-node-in-a-bst/description/
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Approach 1: TC: O(logN), SC: O(logN)
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root) return root;
+        if(!root) return NULL;
         if(root->val > key) {
             root->left = deleteNode(root->left, key);
         }
@@ -24,20 +14,59 @@ public:
             root->right = deleteNode(root->right, key);
         }
         else {
-            // No children scenario
             if(!root->left && !root->right) return NULL;
-            //1 child scenario
-            if(!root->left || !root->right) {
-                return root->left != NULL ? root->left : root->right;
-            }
-            // 2 children scenario
-            TreeNode* left = root->left;
-            while(left && left->right) left = left->right;
-            root->val = left->val;
-            root->left = deleteNode(root->left, left->val);
+            if(!root->left || !root->right) return root->left == NULL ? root->right : root->left;
+            TreeNode* temp = root->left;
+            while(temp && temp->right) temp = temp->right;
+            root->val = temp->val;
+            root->left = deleteNode(root->left, temp->val);
         }
         return root;
     }
 };
 
-// TC: O(N), SC: O(H)
+
+// Approach 2: TC:O(logN), SC: O(1)
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return NULL;
+        TreeNode* node = root;
+        TreeNode* prev = NULL;
+        while(node && node->val != key) {
+            prev = node;
+            if(node->val > key) node = node->left;
+            else if(node->val < key) node = node->right;
+        }
+        if(node != NULL && node->val == key) {
+            if(!node->left && !node->right) { // 0 Children Case
+                if(!prev) return NULL;
+                if(prev->left == node) prev->left = NULL;
+                else prev->right = NULL;
+            }
+            else if(!node->left || !node->right) { // 1 Child Case
+                TreeNode* ret = node->left == NULL ? node->right : node->left;
+                if(!prev) return ret;
+                if(prev->left == node) prev->left = ret;
+                else prev->right = ret;
+            }
+            else { // 2 Children Case
+                TreeNode* temp = node->left;
+                prev = node;
+                while(temp && temp->right) {
+                    prev = temp;
+                    temp = temp->right;
+                }
+                if(prev != node) {
+                    prev->right = temp->left;
+                }
+                else { //Edge Case
+                    node->left = temp->left;
+                }
+                node->val = temp->val;
+            }
+        }
+        
+        return root;
+    }
+};
