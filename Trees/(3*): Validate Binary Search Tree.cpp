caab1a4efd97c750bbbca30b3cbbdf97e688f0a9
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-04-10 14:28:47 -0700

    Update (3*): Validate Binary Search Tree.cpp

--- Trees/(3*): Validate Binary Search Tree.cpp
+++ Trees/(3*): Validate Binary Search Tree.cpp
@@ -3,40 +3,30 @@
 * NOTE: Prefer the iterative versions of Pre / In / Post order traversals
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         if(!root) return true;
         if(!root->left && !root->right) return true;
+
+        TreeNode* prev = NULL;
         stack<TreeNode*> stack;
-        TreeNode* prevNode = NULL;
-        while(root || !stack.empty()) {
+
+        while(!stack.empty() || root) {
             while(root) {
                 stack.push(root);
                 root = root->left;
             }
             root = stack.top();
             stack.pop();
-            if(prevNode && prevNode->val >= root->val) {
-                return false;
-            }
-            prevNode = root;
+            if(prev && prev->val >= root->val) return false;
+            prev = root;
             root = root->right;
         }
+
         return true;
     }
 };
 
 
-// TC: O(N), SC: O(H)
+// TC: O(N), SC: O(N). O(N) complexity for BST in the worst case scenario i.e, a skewed BST
