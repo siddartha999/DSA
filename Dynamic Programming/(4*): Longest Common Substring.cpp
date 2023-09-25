c788971359d692d2ebac0b28f662ce61619e349f
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-09-25 10:13:10 -0700

    Update (4*): Longest Common Substring.cpp

--- Dynamic Programming/(4*): Longest Common Substring.cpp
+++ Dynamic Programming/(4*): Longest Common Substring.cpp
@@ -64,3 +64,69 @@ int lcs(string &str1, string &str2){
     }
     return maxVal;
 }
+
+
+
+
+
+
+
+// Brute Force Approach. TC: O(2^N * 2^M), SC: O(N + M)
+int helper(string&, int, string&, int, int&);
+
+int lcs(string &str1, string &str2){
+    int maxVal = 0;
+    helper(str1, 0, str2, 0, maxVal);
+    return maxVal;
+}
+
+int helper(string& a, int i, string& b, int j, int& maxVal) {
+    if(i >= a.size() || j >= b.size()) return 0;
+    
+    int val = 0;
+    
+    if(a[i] == b[j]) {
+        val = 1 + helper(a, i + 1, b, j + 1, maxVal);
+        maxVal = max(maxVal, val);
+    }
+    
+    helper(a, i, b, j + 1, maxVal); 
+    helper(a, i + 1, b, j, maxVal);
+    
+    return val;
+}
+
+
+
+
+
+
+// Memoized Approach. TC: O(N * M), SC: O(N * M) + O(N + M)
+int helper(string&, int, string&, int, int&, vector<vector<int>>&);
+
+int lcs(string &str1, string &str2){
+    vector<vector<int>> cache(str1.size(), vector<int> (str2.size(), -1));
+    int maxVal = 0;
+    helper(str1, 0, str2, 0, maxVal, cache);
+    return maxVal;
+}
+
+int helper(string& a, int i, string& b, int j, int& maxVal, vector<vector<int>>& cache) {
+    if(i >= a.size() || j >= b.size()) return 0;
+    
+    if(cache[i][j] != -1) return cache[i][j];
+    
+    int val = 0;
+    
+    
+    if(a[i] == b[j]) {
+        val = 1 + helper(a, i + 1, b, j + 1, maxVal, cache);
+        maxVal = max(maxVal, val);
+    }
+    
+    helper(a, i, b, j + 1, maxVal, cache); 
+    helper(a, i + 1, b, j, maxVal, cache);
+    
+    cache[i][j] = val;
+    return val;
+}
