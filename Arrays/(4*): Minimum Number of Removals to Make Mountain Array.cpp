65601c15d6790ed57540854ed3ef52c3f047f1f1
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2023-10-21 13:46:16 -0700

    Create (4*): Minimum Number of Removals to Make Mountain Array.cpp

---
+++ Arrays/(4*): Minimum Number of Removals to Make Mountain Array.cpp
@@ -0,0 +1,34 @@
+/**
+* Problem Link: https://leetcode.com/problems/minimum-number-of-removals-to-make-mountain-array/description/
+**/
+
+// Approach 1: TC: O(N^2), SC: O(N). Not the most optimal solution, there are O(NlogN) solutions out there
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        vector<int> lis(nums.size(), 1);
+        vector<int> lds(nums.size(), 1);
+
+        for(int i = 1; i < nums.size(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) lis[i] = max(lis[i], 1 + lis[j]);
+            }
+        }
+
+        for(int i = nums.size() - 2; i >= 0; i--) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] > nums[j]) lds[i] = max(lds[i], 1 + lds[j]);
+            }
+        }
+
+        int minRemovals = nums.size();
+        for(int i = 0; i < nums.size(); i++) {
+            if(lis[i] == 1 || lds[i] == 1) continue;
+            int mountainLength = lis[i] + lds[i] - 1;
+            int removals = nums.size() - mountainLength;
+            minRemovals = min(minRemovals, removals);
+        }
+
+        return minRemovals;
+    }
+};
