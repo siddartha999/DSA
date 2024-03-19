b8dda767474616731364c41263058f3fbe42a3f9
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2024-03-18 17:58:18 -0700

    Create (4*): Koko Eating Bananas.cpp

---
+++ BinarySearch/(4*): Koko Eating Bananas.cpp
@@ -0,0 +1,35 @@
+/**
+* https://leetcode.com/problems/koko-eating-bananas/description/
+* Reference: https://www.youtube.com/watch?v=qyfekrNni90&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=13
+**/
+
+// TC: O(N) * O(log(maxElement))
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int minVal = 1, maxVal = piles[0];
+        for(int pile: piles) {
+            minVal = min(minVal, pile);
+            maxVal = max(maxVal, pile);
+        }
+
+        int left = minVal, right = maxVal;
+        int res = maxVal;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            long long sumHours = 0;
+            for(int pile: piles) {
+                sumHours += (pile / mid) + (pile % mid != 0 ? 1 : 0);
+            }
+            if(sumHours > h) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+                res = min(res, mid);
+            }
+        }
+
+        return res;
+    }
+};
