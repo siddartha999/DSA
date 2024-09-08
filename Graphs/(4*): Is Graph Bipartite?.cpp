8f458ea090e33cde4d8d6841b44c19052c026002
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2024-09-08 09:26:36 -0700

    Update (4*): Is Graph Bipartite?.cpp

--- Graphs/(4*): Is Graph Bipartite?.cpp
+++ Graphs/(4*): Is Graph Bipartite?.cpp
@@ -1,5 +1,6 @@
 /**
-* Problem link: https://leetcode.com/problems/is-graph-bipartite/description/
+* Problem link 1: https://leetcode.com/problems/is-graph-bipartite/description/
+* Problem link 2: https://www.geeksforgeeks.org/problems/bipartite-graph/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 **/
 
 // Approach 1: BFS. TC: O(V + E), SC: O(V + E)
@@ -30,4 +31,42 @@ public:
    
         return true;
     }
+};
+
+
+
+// Approach: DFS. TC: O(V + E), SC: O(V + E)
+class Solution {
+public:
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<char> color(V, 'd');
+	    for(int vertex = 0; vertex < V; vertex++) {
+	        if(color[vertex] != 'd') continue;
+	        if(!isValid(adj, color, V, vertex)) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+private:
+    bool isValid(vector<int> adj[], vector<char>& color, int V, int vertex) {
+        if(vertex >= V || adj[vertex].size() == 0) {
+            return true;
+        }
+        
+        if(color[vertex] == 'd') color[vertex] = 'a';
+        int adjacentVertexColor = (color[vertex] == 'a') ? 'b' : 'a';
+        for(int idx = 0; idx < adj[vertex].size(); idx++) {
+            int adjacentVertex = adj[vertex][idx];
+            if(color[adjacentVertex] != 'd') {
+                if(color[adjacentVertex] != adjacentVertexColor) return false;
+                continue;
+            }
+            color[adjacentVertex] = adjacentVertexColor;
+            if(!isValid(adj, color, V, adjacentVertex)) return false;
+        }
+        return true;
+    }
+
 };
