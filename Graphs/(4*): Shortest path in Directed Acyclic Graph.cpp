55ca5cab06a1ed3cc8121645284090e8298215af
siddartha999 <37662337+siddartha999@users.noreply.github.com>
2024-09-15 13:39:37 -0700

    Update (4*): Shortest path in Directed Acyclic Graph.cpp

--- Graphs/(4*): Shortest path in Directed Acyclic Graph.cpp
+++ Graphs/(4*): Shortest path in Directed Acyclic Graph.cpp
@@ -48,3 +48,57 @@ private:
         }
     }
 };
+
+
+
+
+
+
+
+//Approach 2: Topological Sort. TC: O(V + E), SC: O(V + E)
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        vector<vector<pair<int, int>>> adj(N);
+        for(int idx = 0; idx < edges.size(); idx++) {
+            int originVertex = edges[idx][0];
+            int receiverVertex = edges[idx][1];
+            adj[originVertex].push_back({receiverVertex, edges[idx][2]});
+        }
+        stack<int> stack;
+        vector<bool> visited(N, false);
+        
+        dfs(adj, stack, visited, 0);   
+        vector<int> res(N, -1);
+        res[0] = 0;
+        while(!stack.empty()) {
+            int vertex = stack.top();
+            stack.pop();
+            for(int idx = 0; idx < adj[vertex].size(); idx++) {
+                int receiverVertex = adj[vertex][idx].first;
+                int weight = adj[vertex][idx].second;
+                if(res[receiverVertex] == -1) {
+                    res[receiverVertex] = res[vertex] + weight;
+                }
+                else if(res[receiverVertex] > res[vertex] + weight) {
+                    res[receiverVertex] = res[vertex] + weight;
+                }
+            }
+        }
+        return res;
+    }
+    
+private: 
+    void dfs(vector<vector<pair<int, int>>>& adj, stack<int>& stack, vector<bool>& visited, int vertex) {
+        if(visited[vertex] == true) {
+            return;
+        }
+        visited[vertex] = true;
+        for(int idx = 0; idx < adj[vertex].size(); idx++) {
+            int receiverVertex = adj[vertex][idx].first;
+            dfs(adj, stack, visited, receiverVertex);
+        }
+        stack.push(vertex);
+    }
+};
